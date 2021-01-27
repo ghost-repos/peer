6faaa85c9168187974d3f3486748f55a3b45402e
reimu <r2hkri@gmail.com>
2021-01-26 22:30:04 -0600

    Add ESLint and update id generation

--- src/index.ts
+++ src/index.ts
@@ -2,83 +2,96 @@ import { createServer } from "http";
 import { createReadStream } from "fs";
 import { Server } from "ws";
 
-let request = 0;
-
 const server = createServer((req, res) => {
-  console.log(request++, req.url);
-  switch (req.url) {
+    switch (req.url) {
     case "/":
-      res.setHeader("content-type", "text/html");
-      createReadStream("./static/index.html").pipe(res);
-      break;
+        res.setHeader("content-type", "text/html");
+        createReadStream("./static/index.html").pipe(res);
+        break;
     case "/adapter.js":
-      res.setHeader("content-type", "application/javascript");
-      createReadStream("./static/adapter.js").pipe(res);
-      break;
+        res.setHeader("content-type", "application/javascript");
+        createReadStream("./static/adapter.js").pipe(res);
+        break;
     default:
-      res.statusCode = 404;
-      res.end();
-  }
-})
+        res.statusCode = 404;
+        res.end();
+    }
+});
 
 server.listen(8080);
 
 const wsServer = new Server({
-  server,
+    server,
 });
 
-let id = 1;
 const clients = new WeakMap();
 const clientIds = new Map();
 
-wsServer.on("connection", (socket) => {
-  socket.on("message", message => {
-    console.log(message);
-    const { op, payload } = JSON.parse(String(message));
-
-    switch (op) {
-      case "IDENTIFY": {
-        clients.set(socket, id);
-        clientIds.set(String(id), socket);
-
-        socket.send(JSON.stringify({
-          op: "IDENTIFY",
-          payload: String(id)
-        }));
-
-        id++;
-        break;
-      }
-      case "MESSAGE": {
-        const { to, message } = payload;
-
-        const from = clients.get(socket);
-        const forwardTo = clientIds.get(String(to));
-
-        forwardTo.send(JSON.stringify({
-          op: "MESSAGE",
-          payload: {
-            from: String(from),
-            to: String(to),
-            message,
-          }
-        }));
-        break;
-      }
+function generateUniqueId() {
+    function generateId() {
+        const words = [["Awesome", "Blue", "Crazy", "Darn", "Edgy"],
+            ["Astute", "Buff", "Cool", "Dead", "Excellent"],
+            ["Animal", "Ball", "Castle", "Dear", "Ear"]];
+        return Array.from({ length: 3 }, () => Math.floor(Math.random() * 5))
+            .map((num, i) => words[i][num]).join("");
     }
-  });
 
-  socket.on("close", () => {
-    const id = clients.get(socket);
-    clients.delete(socket);
-    clientIds.delete(id);
-    console.log(`${id} socket close`);
-  });
+    let id;
+    do {
+        id = generateId();
+    } while (clientIds.has(id));
 
-  socket.on("error", () => {
-    const id = clients.get(socket);
-    clients.delete(socket);
-    clientIds.delete(id);
-    console.log(`${id} socket error`);
-  });
+    return id;
+}
+
+wsServer.on("connection", (socket) => {
+    socket.on("message", (data) => {
+        console.log(data);
+        const { op, payload } = JSON.parse(String(data));
+
+        switch (op) {
+        case "IDENTIFY": {
+            const newId = generateUniqueId();
+            clients.set(socket, newId);
+            clientIds.set(newId, socket);
+
+            socket.send(JSON.stringify({
+                op: "IDENTIFY",
+                payload: newId,
+            }));
+            break;
+        }
+        case "MESSAGE": {
+            const { to, message } = payload;
+
+            const from = clients.get(socket);
+            const forwardTo = clientIds.get(String(to));
+
+            forwardTo.send(JSON.stringify({
+                op: "MESSAGE",
+                payload: {
+                    from: String(from),
+                    to: String(to),
+                    message,
+                },
+            }));
+            break;
+        }
+        default:
+        }
+    });
+
+    socket.on("close", () => {
+        const id = clients.get(socket);
+        clients.delete(socket);
+        clientIds.delete(id);
+        console.log(`${id} socket close`);
+    });
+
+    socket.on("error", () => {
+        const id = clients.get(socket);
+        clients.delete(socket);
+        clientIds.delete(id);
+        console.log(`${id} socket error`);
+    });
 });
